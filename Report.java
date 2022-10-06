4b0cf27f2a156a9f9f25eb344eacad669b1b39fc
AyshaSiddeeqa <114843786+AyshaSiddeeqa@users.noreply.github.com>
2022-10-06 20:23:51 +0800

    Add files via upload

---
+++ Report.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StudenNew Object=new StudenNew();
+System.out.println(  Object.Studentname );
+System.out.println(Object.cgpa);
+System.out.println(Object.Collegename);
+System.out.println(Object.markscored);
+System.out.println(Object.Rollnumber);
+
+
+	}}
+
+		
+
+		
+		
+		
+
---
+++ StudenNew.java
@@ -0,0 +1,9 @@
+package week1.day1;
+
+public class StudenNew {
+String Studentname="Aysha";
+int Rollnumber=1234;
+String Collegename= "SIET";
+int markscored= 120;
+float cgpa= 79.8f;
+}
